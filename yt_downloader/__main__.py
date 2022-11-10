97f15128ec20eb795ed202c517515c26fa5a1573
Rafal Pogorzelski <raf88pogo@gmail.com>
2022-11-10 21:57:59 +0100

    add dist/ to gitignore, fix error in __main__

--- yt_downloader/__main__.py
+++ yt_downloader/__main__.py
@@ -7,13 +7,9 @@ from yt_downloader.yt_dl import (download_single_video, download_full_playlist,
 
 
 # TODO runt linter or something
-def parse_args(args):
+def parse_args():
     parser = argparse.ArgumentParser(
         description="youtube downloader")
-    # If all arg are omitted displays simple usage
-    if not args:
-        parser.print_help()
-        exit(0)
 
     parser.add_argument("-v", "--video", type=str, help="url for the video")
 
@@ -21,7 +17,7 @@ def parse_args(args):
                         help="url for the playlist")
 
     parser.add_argument("-pi", "--playlistInfo", type=str,
-                        help="""this flag allows to download information 
+                        help="""This flag allows to download information 
                         like title, availability of a playlist videos and 
                         they respective links""")
 
@@ -29,25 +25,30 @@ def parse_args(args):
                         help="""The output directory for the downloaded 
                         stream. Default is current working directory""")
 
-    parser.add_argument("-nd", "--newDirectory", help="""if you want to 
+    parser.add_argument("-nd", "--newDirectory", help="""If you want to 
     create a new directory named as the channel where the video will be 
     saved""", action="store_true")
+
+    # this prevent to jump to the inputs in main() if all arg are omitted
+    if len(sys.argv) == 1:
+        parser.print_usage()
+        exit(0)
 
     return parser.parse_args()
 
 
-def main(argv):
-    args = parse_args(argv)
+def main():
+    args = parse_args()
 
     if args.video:
         download_single_video(args.video, args.target, args.newDirectory)
     if args.playlist:
         download_full_playlist(args.playlist, args.target)
-    if args.info:
-        playlist_info = playlist_title_url(args.info)
+    if args.playlistInfo:
+        playlist_info = playlist_title_url(args.playlistInfo)
         for video_info in playlist_info:
             print(video_info)
 
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main()
